2b40dc20911c45fcf0f3b3a6dab27c20730464e1
salmanjahani1 <163454198+salmanjahani1@users.noreply.github.com>
2024-05-31 15:24:35 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,48 +1,19 @@
-from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
-
 from linear_reg_model import Linear_Reg_Model
 import joblib
-import pandas as pd
-import sys
+import json
 
-app = Flask(__name__)
-api = Api(app)
-
-
-class HomePage (Resource):
-    def get(self):
-        return 'Discovery Center Mission'
+def main(event, context):
+    # message = "Hello World from the Kyma Function "+context['function-name']+" running on "+context['runtime']+ "!";
+    # requestData = event['extensions']['request']
 
 
-class RunModel (Resource):
+    with open('./sample_data.json', 'r') as file:
+        json_data = json.load(file)
 
-    def post(self):
-        model_status = "Failure"
+    loaded_model = joblib.load("linear_model_model.pkl")
+    model = Linear_Reg_Model(loaded_model)
+    results = model.predict(json_data)
+    print(results)
+    result = {'predictions': results.tolist()}
 
-        try:
-            json_data = request.get_json(force=True)
-            # print(json_data)
-            loaded_model = joblib.load("linear_model_model.pkl")
-            model = Linear_Reg_Model(loaded_model)
-            results = model.predict(json_data)
-            print(results)
-            # model.run_model(model)
-            model_status = "Success"
-
-        except Exception as e:
-            print((str(e)), file=sys.stderr)
-        # finally:
-        #     msg = self.writeResult(str(model_status))
-        print(model_status)
-        result = {'predictions': results.tolist()}
-        return result
-
-
-api.add_resource(HomePage, '/')
-api.add_resource(RunModel, '/predict')
-
-
-# if __name__ == '__main__':
-def main():
-    app.run('0.0.0.0', '80', debug=True)
+    return results
